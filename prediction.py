86697641cbb30a477b9f230f2b9702c48d9579e3
royang2012 <royang2012@hotmail.com>
2016-06-13 23:16:38 -0400

    weighted sum technical with quadratic terms

--- prediction.py
+++ prediction.py
@@ -2,4 +2,6 @@ import numpy as np
 
 
 def prediction(pastData):
+    # create features
+
     return np.mean(pastData[14:23])
--- technical_eval.py
+++ technical_eval.py
@@ -2,32 +2,60 @@ import pandas as pd
 import numpy as np
 from prediction import prediction
 import heapq
+from sklearn import linear_model
+import itertools
 dfrm = pd.read_csv('./resources/monthly_2011_2015.csv')
 dfrm = dfrm.dropna(axis=1)
 diffData = dfrm.ix[:, 2:467].pct_change(axis=0).dropna()
 diffData.to_csv('./resources/pct_change.csv')
 (rowNum, colNum) = diffData.shape
+inFeature = np.zeros((rowNum * colNum, 27))
+inReturn = np.zeros(rowNum * colNum)
+# Training
+k = 0
+z = 0
+for i in range(rowNum - 36, rowNum - 25):
+    for j in range(0, colNum):
+        inFeature[k, 0:6] = diffData.ix[i - 6:i - 1, j].as_matrix()
+        # inFeature[k, 6:12] = np.dot(inFeature[k, 0:6], inFeature[k, 0:6])
+        for x in range(0, 6):
+            for y in range(x, 6):
+                inFeature[k, 6+z] = inFeature[k, x] * inFeature[k, y]
+                z += 1
+        inReturn[k] = diffData.ix[i, j]
+        z = 0
+        k += 1
+clf = linear_model.LinearRegression()
+clf.fit(inFeature, inReturn)
+print inFeature[0:1, 0:28]
+print clf.coef_
+# Testing
+testFeature = np.zeros((colNum, 27))
+testReturn = np.zeros(colNum)
 ret = np.zeros(colNum)
 grossRet = np.zeros(12)
 totalRet = 1
 k = 0
-for i in range(rowNum-12, rowNum-0):
+z = 0
+for i in range(rowNum - 24, rowNum - 12):
     for j in range(0, colNum):
-        ret[j] = prediction(diffData.ix[i-25:i-1, j].as_matrix())
+        testFeature[j, 0:6] = diffData.ix[i - 6:i - 1, j].as_matrix()
+        for x in range(0, 6):
+            for y in range(x, 6):
+                testFeature[k, 6+z] = testFeature[k, x] * testFeature[k, y]
+                z += 1
+        z = 0
 
-    indices = heapq.nlargest(10, range(len(ret)), ret.take)
-    print indices
+        testReturn[j] = np.dot(clf.coef_, testFeature[j, 0:27])
+    indices = heapq.nlargest(10, range(len(testReturn)), testReturn.take)
     grossRet[k] = diffData.ix[i, indices].mean()
-    print grossRet[k]
-    totalRet *= (1+grossRet[k])
+    totalRet *= (1 + grossRet[k])
     k += 1
+
+    # print indices
+    # grossRet[k] = diffData.ix[i, indices].mean()
+    # print grossRet[k]
+    # totalRet *= (1 + grossRet[k])
+    # k += 1
 print totalRet
-
-
-
-
-
-
-
-
-
+print grossRet
